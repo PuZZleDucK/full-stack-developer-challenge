1e6a311cb9a8c027591f41ce458d95c1b6877041
PuZZleDucK <puzzleduck@gmail.com>
2018-12-09 14:21:56 +1100

    Test: book model

--- abapi/tests.py
+++ abapi/tests.py
@@ -15,3 +15,10 @@ class ModelTest(TestCase):
 	def test_author_model_str(self):
 		author = Author(first_name="author-first", last_name="author-last")
 		self.assertEqual("author-last, author-first", str(author))
+
+	def test_book_model_creation(self):
+		author = Author(first_name="author-first", last_name="author-last")
+		book = Book(name="test-book", isbn="0137129297", author=author)
+		self.assertEqual("test-book", book.name)
+		self.assertEqual("0137129297", book.isbn)
+		self.assertEqual("author-last, author-first", book.author)
