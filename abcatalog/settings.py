184a7820a734c75b0b939ed9386b5b2d1b7fb4b2
PuZZleDucK <puzzleduck@gmail.com>
2018-12-09 12:17:35 +1100

    fix: set static root for heroku

--- abcatalog/settings.py
+++ abcatalog/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
