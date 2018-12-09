8db6e79c3ad26a4481af75151e7a4cd4c24d0196
PuZZleDucK <puzzleduck@gmail.com>
2018-12-09 17:54:02 +1100

    Feature: authors list api

--- abapi/urls.py
+++ abapi/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from .models import Author
 from .views import AuthorViewSet
+from . import views
 
 router = routers.DefaultRouter()
 router.register('author', AuthorViewSet)
 
 urlpatterns = [
+    url('^authors/', AuthorViewSet.as_view({'get': 'list'})),
     url('', include(router.urls)),
 ]
--- abapi/views.py
+++ abapi/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from .models import Author
 from rest_framework import viewsets
 from .searializers import AuthorSerializer
+from rest_framework.response import Response
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+    def list(self, request):
+        queryset = Author.objects.all()
+        serializer = AuthorSerializer(queryset, many=True)
+        return Response(serializer.data)
